83be3cca347f1cf60ca7b2292504031a7f8a2998
vins <vinsdagaraga@gmail.com>
2026-02-23 21:29:09 +0800

    Add support for Cloud Connection Strings (Railway)

--- server/db.js
+++ server/db.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const dbConfig = process.env.MYSQL_URL || {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME || 'osaconnect_db',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-});
+};
+
+const pool = mysql.createPool(dbConfig);
 
 module.exports = pool.promise();
